fb2f4bcfaa5f5fa87d08a5df0a0969f996b1b320
aomsin <eng@MacEng.local>
2022-12-03 03:21:39 +0700

    update

--- app.js
+++ app.js
@@ -14,20 +14,42 @@ app.listen(PORT, () => {
 
 // create connection to database
 // the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
-const db = mysql.createConnection({
+var db = {
   host: "us-cdbr-east-06.cleardb.net",
   user: "bf3a9dd6fdeed5",
   password: "0d4be3cf",
   database: "heroku_26310da32c906f9",
-});
+};
 
-// connect to database
-db.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log("Connected to database");
-});
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db); // Recreate the connection, since
+  // the old one cannot be reused.
+
+  connection.connect(function (err) {
+    // The server is either down
+    if (err) {
+      // or restarting (takes a while sometimes).
+      console.log("error when connecting to db:", err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    } // to avoid a hot loop, and to allow our node script to
+  }); // process asynchronous requests in the meantime.
+  // If you're also serving http, display a 503 error.
+  connection.on("error", function (err) {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      // Connection to the MySQL server is usually
+      handleDisconnect(); // lost due to either server restart, or a
+    } else {
+      // connnection idle timeout (the wait_timeout
+      throw err; // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
 global.db = db;
 
 // configure middleware
