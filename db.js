0b6292bdf7499451ff9e295e9f1afbdbc0dc28ba
ABCz <58149374+engasnm111@users.noreply.github.com>
2022-12-04 23:34:56 +0700

    Updated

--- db.js
+++ db.js
@@ -1,27 +1,12 @@
 const mysql = require("mysql");
+const dbConfig = require("dbconfig.js");
 
 var db = mysql.createPool({
-  connectionLimit: 4,
-  host: "us-cdbr-east-06.cleardb.net",
-  user: "bf3a9dd6fdeed5",
-  password: "0d4be3cf",
-  database: "heroku_26310da32c906f9",
-});
-
-db.getConnection((err, connection) => {
-  if (err) {
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      console.error("Database connection was closed.");
-    }
-    if (err.code === "ER_CON_COUNT_ERROR") {
-      console.error("Database has too many connections.");
-    }
-    if (err.code === "ECONNREFUSED") {
-      console.error("Database connection was refused.");
-    }
-  }
-  if (connection) connection.release();
-  return;
+  connectionLimit: 10,
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB,
 });
 
 module.exports = db;
