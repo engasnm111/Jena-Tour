4fab96d58da6b7d10ac34d42698fa92e4172b358
ABCz <58149374+engasnm111@users.noreply.github.com>
2022-12-04 21:38:53 +0700

    Updated

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-var mysql = require("mysql2");
+var mysql = require("mysql");
 
 var db = mysql.createPool({
   connectionLimit: 10,
@@ -6,16 +6,6 @@ var db = mysql.createPool({
   user: "bf3a9dd6fdeed5",
   password: "0d4be3cf",
   database: "heroku_26310da32c906f9",
-});
-
-db.getConnection(function (err, connection) {
-  connection.query("SELECT id FROM map", function (err, rows) {
-    console.log(db._freeConnections.indexOf(connection)); // -1
-
-    connection.release();
-
-    console.log(db._freeConnections.indexOf(connection)); // 0
-  });
 });
 
 Module.exports = db;
