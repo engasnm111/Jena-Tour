cadd855d0fddaf9fd87a7df4995d795d7e38825c
ABCz <58149374+engasnm111@users.noreply.github.com>
2022-12-05 00:09:45 +0700

    Updated

--- db.js
+++ db.js
@@ -1,24 +1,13 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
-var db = mysql.createPool({
-  connectionLimit: 10,
-  host: "us-cdbr-east-06.cleardb.net",
-  user: "bf3a9dd6fdeed5",
-  password: "0d4be3cf",
-  database: "heroku_26310da32c906f9",
-});
-
-db.getConnection(function (err, connnection) {
-  // execute query
-  // ...
-  connnection.release();
-});
-
-pool.end(function (err) {
-  if (err) {
-    return console.log(err.message);
-  }
-  // close all connections
-});
+var db = mysql
+  .createPool({
+    connectionLimit: 10,
+    host: "us-cdbr-east-06.cleardb.net",
+    user: "bf3a9dd6fdeed5",
+    password: "0d4be3cf",
+    database: "heroku_26310da32c906f9",
+  })
+  .promise();
 
 module.exports = db;
