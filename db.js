11fe90340caaea110bac2073dd24cae2ff9309b7
ABCz <58149374+engasnm111@users.noreply.github.com>
2022-12-04 23:29:27 +0700

    Updated

--- db.js
+++ db.js
@@ -1,40 +1,27 @@
 const mysql = require("mysql");
 
-var db_config = {
+var db = mysql.createPool({
   connectionLimit: 4,
   host: "us-cdbr-east-06.cleardb.net",
   user: "bf3a9dd6fdeed5",
   password: "0d4be3cf",
   database: "heroku_26310da32c906f9",
-};
+});
 
-var db;
-
-function handleDisconnect() {
-  db = mysql.createConnection(db_config); // Recreate the connection, since
-  // the old one cannot be reused.
-
-  db.connect(function (err) {
-    // The server is either down
-    if (err) {
-      // or restarting (takes a while sometimes).
-      console.log("error when connecting to db:", err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    } // to avoid a hot loop, and to allow our node script to
-  }); // process asynchronous requests in the meantime.
-  // If you're also serving http, display a 503 error.
-  db.on("error", function (err) {
-    console.log("db error", err);
+db.getConnection((err, connection) => {
+  if (err) {
     if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      // Connection to the MySQL server is usually
-      handleDisconnect(); // lost due to either server restart, or a
-    } else {
-      // connnection idle timeout (the wait_timeout
-      throw err; // server variable configures this)
+      console.error("Database connection was closed.");
     }
-  });
-}
-
-handleDisconnect();
+    if (err.code === "ER_CON_COUNT_ERROR") {
+      console.error("Database has too many connections.");
+    }
+    if (err.code === "ECONNREFUSED") {
+      console.error("Database connection was refused.");
+    }
+  }
+  if (connection) connection.release();
+  return;
+});
 
 module.exports = db;
