c08e991c81d2fdbc4c0788bd3104c4df7e13fe36
ABCz <58149374+engasnm111@users.noreply.github.com>
2022-12-05 20:31:09 +0700

    update

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql");
 
-// var db = mysql.createPool({
-//   host: "us-cdbr-east-06.cleardb.net",
-//   user: "bece4ed5f2fc4d",
-//   password: "9e80a0bb",
-//   database: "heroku_962c2ea55add6f7",
-// });
-
 var db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "utcc",
+  host: "us-cdbr-east-06.cleardb.net",
+  user: "bece4ed5f2fc4d",
+  password: "9e80a0bb",
+  database: "heroku_962c2ea55add6f7",
 });
+
+// var db = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "utcc",
+// });
 
 db.getConnection((err, connection) => {
   if (err) {
